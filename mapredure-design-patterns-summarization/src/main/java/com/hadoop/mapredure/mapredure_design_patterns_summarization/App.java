3718be2f81f1ae3c0c6da7e3759ff13c697c7e83
Vincent-Poon <mmcpanzhichun@gmail.com>
2015-03-15 23:34:44 +0800

    summarization/概要模式

---
+++ mapredure-design-patterns-summarization/src/main/java/com/hadoop/mapredure/mapredure_design_patterns_summarization/App.java
@@ -0,0 +1,13 @@
+package com.hadoop.mapredure.mapredure_design_patterns_summarization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
