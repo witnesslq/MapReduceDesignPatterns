9c14ad37f8f70f866458f2550609799ac661d761
Vincent <mmcpanzhichun@gmail.com>
2015-03-19 13:50:36 +0800

    修改输入格式为KV结构

--- mapredure-design-patterns-summarization/src/main/java/com/MRdesignpatterns/summarization/MaxMinCount/MaxMinCountDriver.java
+++ mapredure-design-patterns-summarization/src/main/java/com/MRdesignpatterns/summarization/MaxMinCount/MaxMinCountDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import com.MRdesignpatterns.summarization.MaxMinCount.MinMaxCountTuple;
@@ -28,7 +29,8 @@ public class MaxMinCountDriver {
 		job.setReducerClass(MinMaxCountReducer.class);
 		job.setOutputKeyClass(IntWritable.class);
 		job.setOutputValueClass(MinMaxCountTuple.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+		KeyValueTextInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
--- mapredure-design-patterns-summarization/src/main/java/com/MRdesignpatterns/summarization/MedianStdDev/MedianStdDevDriver.java
+++ mapredure-design-patterns-summarization/src/main/java/com/MRdesignpatterns/summarization/MedianStdDev/MedianStdDevDriver.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MedianStdDevDriver {
@@ -23,7 +24,8 @@ public class MedianStdDevDriver {
 		job.setReducerClass(MedianStandardRedure.class);
 		job.setOutputKeyClass(IntWritable.class);
 		job.setOutputValueClass(DoubleWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
+		job.setInputFormatClass(KeyValueTextInputFormat.class);
+		KeyValueTextInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
